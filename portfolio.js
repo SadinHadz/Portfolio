f576fe17baae7f6391a278a41a0046a6a98df6ac
SadinHadz <120284727+SadinHadz@users.noreply.github.com>
2022-12-11 20:51:23 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav2").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
